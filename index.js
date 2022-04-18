3de303b9676996f2bec16abe841fe26895d418d8
zisan <uzummzisann@gmail.com>
2022-04-18 22:34:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+// console.log(mondayWork("hello"));
+function wrapAdjective(adjective = "*") {
+  return function (param = "special") {
+    return `You are ${adjective}${param}${adjective}!`;
+  };
+}
+
+// let result = wrapAdjective('*')
+// let emphatic = result("a hard worker")
+// expect(emphatic).to.equal("You are *a hard worker*!")
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
